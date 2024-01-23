20142498f6884b9e211bbfc79baa58dba9b66271
icsboyx <obladio28@gmail.com>
2024-01-23 17:50:10 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use raylib::{misc::AsF32, prelude::*};
+
+struct Entity {
+    x: f32,
+    y: f32,
+    radius: i32,
+    canvas: (i32, i32),
+}
+impl Entity {
+    fn new(x: f32, y: f32, radius: i32, canvas: (i32, i32)) -> Self {
+        Self {
+            x,
+            y,
+            radius,
+            canvas,
+        }
+    }
+    fn get_x(&self) -> f32 {
+        self.x
+    }
+    fn set_x(&mut self, x: f32) {
+        if x > self.canvas.0 as f32 + (self.radius / 2) as f32 {
+            self.x = 0.0;
+        } else if x < 0.0 - (self.radius / 2) as f32 {
+            self.x = self.canvas.0 as f32;
+        } else {
+            self.x = x;
+        }
+    }
+    fn get_y(&self) -> f32 {
+        self.y
+    }
+    fn set_y(&mut self, y: f32) {
+        if y > self.canvas.1 as f32 + (self.radius / 2) as f32 {
+            self.y = 0.0;
+        } else if y < 0.0 - (self.radius / 2) as f32 {
+            self.y = self.canvas.1 as f32;
+        } else {
+            self.y = y;
+        }
+    }
+    fn draw(&self, d: &mut RaylibDrawHandle) {
+        d.draw_circle(self.x as i32, self.y as i32, self.radius as f32, Color::RED);
+    }
+}
+
+fn main() {
+    let w = 640;
+    let h = 480;
+
+    let mut entity = Entity::new((h / 2) as f32, (w / 2) as f32, 40, (w, h));
+
+    let (mut rl, thread) = raylib::init().size(w, h).title("Hello, World").build();
+    enum ArrowKey {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    impl ArrowKey {
+        fn to_raylib_key(&self) -> KeyboardKey {
+            match self {
+                ArrowKey::Up => KeyboardKey::KEY_UP,
+                ArrowKey::Down => KeyboardKey::KEY_DOWN,
+                ArrowKey::Left => KeyboardKey::KEY_LEFT,
+                ArrowKey::Right => KeyboardKey::KEY_RIGHT,
+            }
+        }
+        fn get_arrows_keys() -> Vec<ArrowKey> {
+            vec![
+                ArrowKey::Up,
+                ArrowKey::Down,
+                ArrowKey::Left,
+                ArrowKey::Right,
+            ]
+        }
+    }
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        entity.draw(&mut d);
+        for key in ArrowKey::get_arrows_keys() {
+            if d.is_key_down(key.to_raylib_key()) {
+                match key {
+                    ArrowKey::Up => entity.set_y(entity.get_y() - 1.0),
+                    ArrowKey::Down => entity.set_y(entity.get_y() + 1.0),
+                    ArrowKey::Left => entity.set_x(entity.get_x() - 1.0),
+                    ArrowKey::Right => entity.set_x(entity.get_x() + 1.0),
+                }
+            }
+        }
+    }
+}
